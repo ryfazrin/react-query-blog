0b814671f8b77f6fef67496ef744c41434a5a545
ryfazrin <riansera515@gmail.com>
2023-04-02 00:33:37 +0700

    created: posts components error https://react.dev/warnings/invalid-hook-call-warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
+import React from "react";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import Posts from './components/Posts';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [postId, setPostId] = React.useState(-1);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        {postId > -1 ? (
+          <p>Post Id</p>
+        ) : (
+          <Posts setPostId={setPostId} />
+        )}
+        <ReactQueryDevtools initialIsOpen />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </QueryClientProvider>
   )
 }
 
---
+++ src/components/Posts.jsx
@@ -0,0 +1,41 @@
+import { useQuery, useQueryClient } from "react-query";
+import axios from "axios";
+
+function usePosts() {
+  return useQuery("posts", async () => {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    return data;
+  });
+}
+
+function Posts({ setPostId }) {
+  const queryClient = useQueryClient();
+  const { status, data, error, isFetching } = usePosts();
+
+  return (<div>
+    <h1>Posts</h1>
+    <div>
+      {status === "loading" ? (
+        <div>Loading...</div>
+      ) : status === "error" ? (
+        <span>Error: {error.message}</span>
+      ) : (
+        <>
+          <div>
+            {data.map((post) => (
+              <p key={post.id}>
+                <a onClick={() => usePosts(post.id)} href="#">{post.title}</a>
+              </p>
+            ))}
+          </div>
+          <div>{isFetching ? "Background Updating..." : " "}</div>
+        </>
+      )}
+    </div>
+  </div>);
+}
+
+export default Posts;
